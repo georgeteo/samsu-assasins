25e071af64123193067d3f9a903dc790f35917ec
George Teo <george.c.teo@gmail.com>
2016-08-21 20:23:08 -0700

    bug fix

--- model/player.py
+++ model/player.py
@@ -90,6 +90,7 @@ class Team(ndb.Model):
     def push(cls, team):
         """ Returns push kill message or [] """
         logging.info("Push start")
+        logging.info("Pushed called for team: {}".format(team))
 
         count_my_team = 0
         for player_id in Team.get_players(team):
@@ -113,18 +114,40 @@ class Team(ndb.Model):
         aggressor_team = Team.get_by_id(team.target_of)
         target_team = Team.get_by_id(team.to_kill)
 
+        logging.info("Before team update:")
+        logging.info("Aggressor: {}".format(aggressor_team))
+        logging.info("This team: {}".format(team))
+        logging.info("Target team: {}".format(target_team))
+
         aggressor_team.to_kill = target_team.key.id()
         aggressor_team.put()
 
+        target_team.target_of = aggressor_team.key.id()
+        target_team.put()
+
+        team.to_kill = ""
+        team.target_of = ""
+        team.put()
+
+        logging.info("Before team update:")
+        logging.info("Aggressor: {}".format(aggressor_team))
+        logging.info("This team: {}".format(team))
+        logging.info("Target team: {}".format(target_team))
+
+        logging.info("Making message")
         message = "Congratulations. Your next target is team {}:\n".format(\
                 target_team.key.id())
         count = 0
         for target_id in Team.get_players(target_team):
             target_player = Player.get_by_id(target_id)
+            logging.info("Looking at target player: {}".format(target_player.realname))
             if target_player.state == "ALIVE":
+                logging.info("He is {}.".format(target_player.state))
                 count += 1
                 message += "{} - {}\n".format(target_player.realname,\
                         target_player.codename)
+            logging.info("He is {}.".format(target_player.state))
+        logging.info("End message make. Message is: {}".format(message))
 
         push_messages = []
         if count == 1 and aggressor_team.to_kill == aggressor_team.key.id():
@@ -132,9 +155,15 @@ class Team(ndb.Model):
             message = "Congratulations. You are the winner."
             push_messages.append((WEI_HAN, "Game is over. Please check that winner got the message."))
 
+        logging.info("Making push messages to send.")
         for aggressor_id in Team.get_players(aggressor_team):
+            logging.info("Looking at {}".format(aggressor_id))
             if Player.get_by_id(aggressor_id).state == "ALIVE":
+                logging.info("He is alive. Append: {}".format((aggressor_id, message)))
                 push_messages.append((aggressor_id, message))
+            logging.info("He is dead.")
+
+        logging.info("Push complete: {}".format(push_messages))
 
         return push_messages
 
